76629acc0918e7e5e3572a6841a6573e99e0b404
verterbert <108976052+verterbert@users.noreply.github.com>
2022-07-13 04:44:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -59,10 +59,6 @@ export default e => {
     });
 
     app.add(mesh);
-    const geometry = new THREE.SphereGeometry( 15, 32, 16 );
-const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-const sphere = new THREE.Mesh( geometry, material );
-app.add( sphere );
   })());
 
   app.updateMatrixWorld();
